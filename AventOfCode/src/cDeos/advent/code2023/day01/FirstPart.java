511d089bd8a95d757955a1bf95153088f393795c
DEIAN ORLANDO PETROVICS <dp189@inlumine.ual.es>
2023-12-01 08:40:54 +0100

    Comiezo prueba01dia01, me ha costado mucho más de lo que me
    gustaría admitir haber llegado hasta el punto en el que me
    
    encuentro ahora

--- AventOfCode/src/cDeos/advent/code2023/day01/FirstPart.java
+++ AventOfCode/src/cDeos/advent/code2023/day01/FirstPart.java
@@ -1,6 +1,61 @@
 package cDeos.advent.code2023.day01;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FirstPart {
-    public static void main(String[] args) {
-        System.out.println("Day 1 - First Part");
-    }
+
+	// private ArrayList<Integer> sumas;
+	private ArrayList<ArrayList<String>> numeros;
+
+	public FirstPart() {
+		this.numeros = new ArrayList<>();
+	}
+
+	static String directorioEntrada = System.getProperty("user.dir") + File.separator + "src" + File.separator + "cDeos"
+			+ File.separator + "advent" + File.separator + "code2023" + File.separator + "day01" + File.separator;
+
+	public boolean load(String nombreArchivo) {
+
+		Scanner scan;
+		try {
+			scan = new Scanner(new File(nombreArchivo));
+		} catch (Exception e) {
+			return false;
+		}
+		while (scan.hasNextLine()) {
+			String linea = scan.nextLine();
+			if (linea.isEmpty())
+				continue;
+			String[] items = linea.split("\\D+");
+			ArrayList<String> listaTemporal = new ArrayList<>();
+			for (String item : items) {
+				if (!item.isEmpty()) {
+					listaTemporal.add(item);
+				}
+			}
+			if (!listaTemporal.isEmpty()) {
+				numeros.add(listaTemporal);
+			}
+
+		}
+
+		scan.close();
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return this.numeros.toString();
+	}
+
+	public static void main(String[] args) {
+
+		FirstPart prueba = new FirstPart();
+		prueba.load(directorioEntrada + "firstInput.txt");
+
+		System.out.println(prueba.toString());
+
+	}
 }
